beb1b4ede232aaeb8cdb3e323f120ab0fef036de
bkj <ben@gophronesis.com>
2017-01-28 14:37:39 -0500

    adding support for 'slow send' which makes this compatable w/ iPython

--- SendText.py
+++ SendText.py
@@ -7,7 +7,7 @@ import tempfile
 
 settings = {}
 
-class SendSelectionCommand(sublime_plugin.TextCommand):
+class _SendSelectionCommand(sublime_plugin.TextCommand):
     @staticmethod
     def escapeString(s):
         s = s.replace('\\', '\\\\')
@@ -19,13 +19,14 @@ class SendSelectionCommand(sublime_plugin.TextCommand):
         prog = settings.get('program')
 
         if prog == "Terminal.app":
+            
             # Remove trailing newline
             selection = selection.rstrip('\n')
+            
             # Split selection into lines
             selection = SendSelectionCommand.escapeString(selection)
-
-            subprocess.call(['osascript', '-e',
-                'tell app "Terminal" to do script "' + selection + '" in window 1'])
+            
+            subprocess.call(['osascript', '-e', 'tell app "Terminal" to do script "' + selection + '" in window 1'])
 
         elif prog == "iTerm":
             # Remove trailing newline
@@ -69,51 +70,38 @@ class SendSelectionCommand(sublime_plugin.TextCommand):
                         file.write(selection)
                         subprocess.call([progpath, '-X', 'stuff', ". %s\n" % (file.name)])
 
-
+class SendSelectionCommand(_SendSelectionCommand):
     def run(self, edit):
         global settings
         settings = sublime.load_settings('SendText.sublime-settings')
-
+        
         # get selection
         selection = ""
         for region in self.view.sel():
             if region.empty():
                 selection += self.view.substr(self.view.line(region))
-                selection = re.sub('^\t*', '', selection) # BKJ edit -- better for Python
-                selection += "\n"
-                # self.advanceCursor(region) # BKJ edit -- this functionality was dumb
+                selection = re.sub('^ *', '', selection)
+                self.send(selection + '\n')
             else:
-                selection += self.view.substr(region) 
-                # selection = selection.split('\n')
-                # self.detab(selection)
-                # selection = '\n'.join(selection)
-                selection += "\n\n"
+                selection += self.view.substr(region)
+                selection = selection.split('\n')
+                self.send('\n'.join(selection))
+                # _ = [self.send(s) for s in selection]
 
-        # BKJ edit -- want to be able to send empty lines
-        # only proceed if selection is not empty
-        # if(selection == "" or selection == "\n"):
-        #     return
 
-        self.send(selection)
-
-    # BKJ addition -- strip tabs from python
-    def detab(self, selection):
-        selection = [x for x in selection if x != '']
-        mintabs = min([len(re.sub('(^\t*).*', '\\1', x)) for x in selection])
-        for m in range(0, mintabs):
-            selection = [re.sub('^\t', '', x) for x in selection]
-        return selection
-
-    # def advanceCursor(self, region):
-    #     (row, col) = self.view.rowcol(region.begin())
-
-    #     # Make sure not to go past end of next line
-    #     nextline = self.view.line(self.view.text_point(row + 1, 0))
-    #     if nextline.size() < col:
-    #         loc = self.view.text_point(row + 1, nextline.size())
-    #     else:
-    #         loc = self.view.text_point(row + 1, col)
-
-    #     # Remove the old region and add the new one
-    #     self.view.sel().subtract(region)
-    #     self.view.sel().add(sublime.Region(loc, loc))
+class SendSelectionSlowCommand(_SendSelectionCommand):
+    def run(self, edit):
+        global settings
+        settings = sublime.load_settings('SendText.sublime-settings')
+        
+        # get selection
+        selection = ""
+        for region in self.view.sel():
+            if region.empty():
+                selection += self.view.substr(self.view.line(region))
+                selection = re.sub('^ *', '', selection)
+                self.send(selection + '\n')
+            else:
+                selection += self.view.substr(region)
+                selection = selection.split('\n')
+                _ = [self.send(s) for s in selection]
